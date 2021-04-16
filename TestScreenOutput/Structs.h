7e40489a79f547b10c3ed92483c5f600ce1a2e90
EvanMilacic <evan.milacic@gmail.com>
2021-04-16 10:58:32 +0200

    Added the Index2 struct.

--- TestScreenOutput/Structs.h
+++ TestScreenOutput/Structs.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <Windows.h>
 
-
 struct RenderState {
 	int height, width;
 	void* memory;
 	BITMAPINFO bitmap_info;
+};
+
+struct Index2 {
+	int i;
+	int j;
+	int data[2]{ i,j };
 };
