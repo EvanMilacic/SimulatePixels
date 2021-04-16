aeabd79afb975b31f58e44a2b7eb04f2333c3695
EvanMilacic <evan.milacic@gmail.com>
2021-04-16 18:47:10 +0200

    Added default material.

---
+++ TestScreenOutput/Default.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Material.h"
+namespace materie {
+
+	class Default : public materie::Material {
+
+	public:
+
+		Default() {
+			type = MatType::Default;
+			color = 0x000000;
+			dynamic = false;
+			combustible = false;
+			soluble = false;
+			hurts = false;
+			density = 0.0f;
+			damage = 0.0f;
+		};
+
+		~Default();
+
+		void UpdateState(void) {};
+		void UpdatePosition(void) {};
+
+	private:
+
+
+	};
+
+} // namespace materie
