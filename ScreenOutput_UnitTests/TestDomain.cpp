d085d4167a849cd8659305d0c918df0c940b98fa
EvanMilacic <evan.milacic@gmail.com>
2023-03-04 11:01:04 +0100

    Changed the unit tests to adapt index2

--- ScreenOutput_UnitTests/TestDomain.cpp
+++ ScreenOutput_UnitTests/TestDomain.cpp
@@ -3,6 +3,8 @@
 #include "../TestScreenOutput/Domain.h"
 #include "../TestScreenOutput/Domain.cpp"
 #include "../TestScreenOutput/Enums.h"
+#include "../TestScreenOutput/FieldData.h"
+#include <iostream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -22,64 +24,50 @@ namespace UnitTests {
 	TEST_CLASS(TestDomain) {
 public:
 	TEST_METHOD(Constructor) {
-		Index_t width = 10;
-		Index_t height = 12;
-		Index_t size = width * height;
-		simulate::Domain testDomain(width, height);
+		FieldData fData = { 10,12,120 };
+		simulate::Domain testDomain(fData);
 
-		Assert::AreEqual(size, testDomain.getSize());
+		Assert::AreEqual(fData.Size, testDomain.getSize());
 	}
 
 	TEST_METHOD(Set2D) {
-		simulate::Domain testDomain(5, 5);
-		testDomain.set(2, 2, MatType::Sand);
-		Assert::AreEqual(MatType::Sand, testDomain.at(2, 2));
-	}
-
-	TEST_METHOD(Set1D) {
-		simulate::Domain testDomain(5, 5);
-		testDomain.set(15, MatType::Sand);
-		Assert::AreEqual(MatType::Sand, testDomain.at(15));
+		FieldData fData = { 5,5,25 };
+		simulate::Domain testDomain(fData);
+		Index2 ind = { 2,2 };
+		testDomain.set(ind, MatType::Sand);
+		Assert::AreEqual(MatType::Sand, testDomain.at(ind));
 	}
 
 	TEST_METHOD(Clear) {
-		simulate::Domain testDomain(5, 5);
-		testDomain.set(24, MatType::Blood);
-		testDomain.clear(24);
-		Assert::AreEqual(testDomain.at(24), MatType::Default);
-	}
-	TEST_METHOD(CalcIndexTesting) {
-		
-		//Test conversion to 1D
-		simulate::Domain test1D(4, 6);
-		test1D.set(3,5, MatType::Sand);
-		Assert::AreEqual(test1D.at(23),MatType::Sand);
-
-		//Test conversion to 2D
-		simulate::Domain test2D(4, 6);
-		test2D.set(23, MatType::Sand);
-		Assert::AreEqual(test2D.at(23), MatType::Sand);
+		FieldData fData = { 5,5,25 };
+		simulate::Domain testDomain(fData);
+		testDomain.set({3,4}, MatType::Blood);
+		testDomain.clear({3,4});
+		Assert::AreEqual(testDomain.at({3,4}), MatType::Default);
 	}
 
 	TEST_METHOD(OnEdgeTest) {
-		simulate::Domain testDomain(4, 6);
-		Assert::IsTrue(testDomain.isOnEdge(0));
-		Assert::IsTrue(testDomain.isOnEdge(4));
-		Assert::IsTrue(testDomain.isOnEdge(8));
-		Assert::IsTrue(testDomain.isOnEdge(23));
-		Assert::IsFalse(testDomain.isOnEdge(5));
+		FieldData fData = {4,6,24};
+		simulate::Domain testDomain(fData);
+		Assert::IsTrue(testDomain.isOnEdge({0,1}));
+		Assert::IsTrue(testDomain.isOnEdge({2,0}));
+		Assert::IsTrue(testDomain.isOnEdge({ 0,5 }));
+		Assert::IsTrue(testDomain.isOnEdge({3,5}));
+		Assert::IsFalse(testDomain.isOnEdge({2,2}));
 	}
 
 	TEST_METHOD(Move) {
 		Index2 start{ 2,2 };
-		simulate::Domain testDomain(5, 5);
+		FieldData fData = { 5,5,25 };
+		simulate::Domain testDomain(fData);
 		testDomain.set(start, MatType::Sand);
 		bool testBool = testDomain.move(start, MoveDirs::Up);
 		Assert::AreEqual(MatType::Sand, testDomain.at(start,MoveDirs::Up));
 	}
 
 	TEST_METHOD(Flip) {
-		simulate::Domain testDomain(4, 6);
+		FieldData fData = { 4,6,24 };
+		simulate::Domain testDomain(fData);
 		Index2 ind1{ 3,5 };
 		Index2 ind2{ 0,0 };
 		testDomain.set(ind1, MatType::Blood);
